79d229ad58a5fbfb9619abdac5ecf0af0e01e4ae
buaalcp <buaalcp@163.com>
2022-05-10 17:56:35 +0800

    delete unused code

--- dy_search.py
+++ dy_search.py
@@ -27,8 +27,6 @@ def begin_search(browser: WebDriver, keyword: str, expect_search_result_num: int
         video = browser.find_element(By.XPATH, f'//*[@id="dark"]/div[2]/div/div[3]/div[1]/ul/li[{i}]')
         browser.execute_script("arguments[0].scrollIntoView();", video)
 
-        # hover_div = video.find_element(By.XPATH, 'div/div/div[3]/div/div/div[1]/div[1]/div/div[3]/div[2]')
-        # webdriver.ActionChains(browser).move_to_element(hover_div).perform()
         WebDriverWait(browser, 30).until(lambda driver: driver.find_element(By.XPATH,
                                                                             f'//*[@id="dark"]/div[2]/div/div[3]/div[1]/ul/li[{i}]/div/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div[1]/xg-controls/xg-inner-controls/xg-right-grid/xg-icon[1]/a')
                                          )
